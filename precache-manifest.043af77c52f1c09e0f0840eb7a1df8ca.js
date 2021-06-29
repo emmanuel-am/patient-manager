57f68ce3a471f3ded5c74a3e7fdd3608971feae2
emmanuel-am <emmanuel_am1@hotmail.com>
2021-06-29 14:51:15 -0500

    Updates

---
+++ precache-manifest.043af77c52f1c09e0f0840eb7a1df8ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f54d5b445ef8ad7049f409a1cdf3cad",
+    "url": "/patient-manager/index.html"
+  },
+  {
+    "revision": "8c05a054b0a1f88e1324",
+    "url": "/patient-manager/static/css/main.6f7657ca.chunk.css"
+  },
+  {
+    "revision": "4eeb61d24d793d114467",
+    "url": "/patient-manager/static/js/2.e0ca3766.chunk.js"
+  },
+  {
+    "revision": "8c05a054b0a1f88e1324",
+    "url": "/patient-manager/static/js/main.a0270bac.chunk.js"
+  },
+  {
+    "revision": "a021fc0e61ad87a07815",
+    "url": "/patient-manager/static/js/runtime-main.13576cc0.js"
+  }
+]);
